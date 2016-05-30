dff4419c61d3ebdbb29d67e4479d9301ffe4633e
kongnara-sophanit <kongnara2303@gmail.com>
2016-05-30 10:06:17 +0700

    test

---
+++ src/WrapperClass/test.java
@@ -0,0 +1,32 @@
+package WrapperClass;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+				//Float x = new Float(5);
+				
+				Double x = new Double(6);
+				
+				  // Returns byte primitive data type
+			      System.out.println( x.byteValue() );
+
+			      // Returns double primitive data type
+			      System.out.println(x.doubleValue());
+
+			      // Returns long primitive data type
+			      System.out.println( x.longValue() );
+			      
+			      Integer number = new Integer(5);
+			      number += 10;
+			      
+			      System.out.println(number.doubleValue());
+			      
+	      byte a = -10 , b = 2;
+	      System.out.println(a+b);
+	      
+	      
+	}
+
+}
